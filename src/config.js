3a3a35ad658c56a58803a8d5e6956f152656cf4e
Abdykali Azat <azat.abdykalli@gmail.com>
2024-10-10 20:31:39 +0500

    first commit

---
+++ src/config.js
@@ -0,0 +1,11 @@
+// config.js
+const config = {
+    // apiUrl: 'http://localhost:3001',
+    apiUrl: 'https://ak-dani-cargo.kz:3001',
+    
+    nameCargo: 'Ak_Dani_Cargo'
+  };
+  
+  
+export default config;
+  
